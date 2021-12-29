d73ab84cde83f3fdfd65c3290755e5590042a262
Rachel Sanham <green_chul@hotmail.com>
2021-12-29 19:26:24 +0000

    WIP

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+
 const artistRouter = require('./routes/artist');
 const albumRouter = require('../src/routes/album');
 
 const app = express();
 
 app.use(express.json());
+
 app.use(artistRouter);
 app.use(albumRouter);
 
 app.get('/', (request, response) => {
-  response.status(200).response.send('Hello World');
+  response.status(200).send('Hello World');
 });
 
 module.exports = app;
