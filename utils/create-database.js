47acffb5fa3ef43c0736458fee6320d22db476a0
Rachel Sanham <green_chul@hotmail.com>
2022-01-13 10:38:02 +0000

    added procfile

--- utils/create-database.js
+++ utils/create-database.js
@@ -12,27 +12,39 @@ const args = process.argv.slice(2)[0];
 const envFile = args === 'test' ? '../.env.test' : '../.env';
 
 // load environment variables from env files
-require('dotenv').config({
-  path: path.join(__dirname, envFile),
-});
+if (args === 'test') {
+  require('dotenv').config({
+    path: path.join(__dirname, envFile),
+  });
+}
 
 // destructure environment variables from process.env
-const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+const {
+  DB_PASSWORD,
+  DB_NAME,
+  DB_USER,
+  DB_HOST,
+  DB_PORT,
+  CLEARDB_DATABASE_URL,
+} = process.env;
 
 // This asyncronous function will run before app
 const setUpDatabase = async () => {
   try {
     // connect to the database
-    const db = await mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      port: DB_PORT,
-    });
+    const db = CLEARDB_DATABASE_URL
+      ? await mysql.createConnection(CLEARDB_DATABASE_URL)
+      : await mysql.createConnection({
+          host: DB_HOST,
+          user: DB_USER,
+          password: DB_PASSWORD,
+          port: DB_PORT,
+        });
 
     // create the database if it doesn't already exist
-    await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
-    await db.query(`USE ${DB_NAME}`);
+    !CLEARDB_DATABASE_URL &&
+      (await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`));
+    !CLEARDB_DATABASE_URL && (await db.query(`USE ${DB_NAME}`));
     await db.query(`CREATE TABLE IF NOT EXISTS Artist (
       id INT PRIMARY KEY auto_increment,
       name VARCHAR(25),
